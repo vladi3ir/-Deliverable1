8923a126623176f36acb40ec0316647c39acf0a4
Vlad <42613754+vladi3ir@users.noreply.github.com>
2019-06-15 23:05:09 -0400

    First commit

---
+++ ConsoleApp1/GettingStarted.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoApplication
+{
+    class Program
+    {
+        //enum Days { Sun, Mon, Tue, wed, thu, fri, sat };
+        static void Main(string[] args)
+        {
+            //Int32 val1 = 5, val2 = 20;
+            //bool status = true;
+
+            // SWITCH STATEMENT
+            //Int32 value = 1;
+            //switch(value)
+            //{
+            //    case 1: Console.WriteLine("Value is 1");
+            //        break;
+            //    case 2: Console.WriteLine("Value is 2");
+            //        break;
+            //    default: Console.WriteLine("Value is different");
+            //        break;
+            //} 
+
+
+            //String message = "The value is ";
+            //Int32 val = 30;
+
+            //String message = "Hello";
+            //Boolean status = true;
+            //double num = 30.33;
+            //Int32 num = 30;
+
+            //Console.Write(message+val);
+            //if (val1 < 10)
+            //{
+            //    Console.WriteLine("Value is less than 10");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("The value is greater than 10");
+            // }
+
+            //Console.WriteLine(val1 + val2);
+            //Console.WriteLine(val1 < val2);
+            //Console.WriteLine(!(status));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoApplication
+{
+    class Program
+    {
+        //enum Days { Sun, Mon, Tue, wed, thu, fri, sat };
+        static void Main(string[] args)
+        {
+            Int32 val1 = 2, val2 = 3;
+            bool status = true;
+            //String message = "The value is ";
+            //Int32 val = 30;
+             //String message = "Hello";
+            //Boolean status = true;
+            //double num = 30.33;
+            //Int32 num = 30;
+
+            //Console.Write(message+val);
+
+            Console.WriteLine(int);
+            Console.WriteLine(val1/val2);
+            Console.WriteLine(!(status));
+
+            Console.ReadKey();
+        }
+    }
+}
